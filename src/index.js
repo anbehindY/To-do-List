0809949e85b9a031af3d365d37a0e94146ae6580
anbehindY <yanminthwin@gmail.com>
2023-03-29 02:09:04 +0630

    Add a function that create tasks populated HTML

--- src/index.js
+++ src/index.js
@@ -18,3 +18,17 @@ const tasksInfo = [
     index: 2,
   },
 ];
+
+const tasksContainer = document.querySelector('.tasks');
+
+// Each item in the tasksInfo array passed through this function and a new array is created
+const task = tasksInfo.map(
+  (taskInfo) => `<div class="listItems">
+                <label for="task${taskInfo.index}">
+                    <input id="task${taskInfo.index}" type="checkbox"> ${taskInfo.description}
+                </label>
+               </div>`,
+);
+
+// Each item in the array created by task function are joined and put inside the tasksContainer
+tasksContainer.innerHTML = task.join('');
