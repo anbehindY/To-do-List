a75afae21524552e716c72c75951b885b3349305
anbehindY <yanminthwin@gmail.com>
2023-03-30 11:54:25 +0630

    Implement add function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import './styles.css';
+import TaskList from './mainFunction.js';
 
-// Array of the tasks' information
-const tasksInfo = [];
-
-const tasksContainer = document.querySelector('.tasks');
-
-// Each item in the tasksInfo array passed through this function and a new array is created
-const task = tasksInfo.map(
-  (taskInfo) => `<div class="listItems">
-                <label for="task${taskInfo.index}">
-                    <input id="task${taskInfo.index}" type="checkbox"> ${taskInfo.description}
-                </label>
-               </div>`,
-);
-
-// Each item in the array created by task function are joined and put inside the tasksContainer
-tasksContainer.innerHTML = task.join('');
+const taskList = new TaskList();
+taskList.render();
---
+++ src/mainFunction.js
@@ -0,0 +1,41 @@
+const tasksContainer = document.querySelector('.tasks');
+const addBtn = document.querySelector('.enter');
+const addText = document.querySelector('#starter');
+
+export default class TaskList {
+  constructor() {
+    this.tasksInfo = [];
+    this.taskKey = 'key';
+  }
+
+  add = () => {
+    const description = addText.value;
+    const completed = false;
+    const index = 0;
+    const taskInfo = {
+      description,
+      completed,
+      index,
+    };
+    this.tasksInfo.push(taskInfo);
+    this.display();
+  }
+
+  display = () => {
+    //  Each item in the tasksInfo array passed through this function and a new array is created
+    const task = this.tasksInfo.map(
+      (info) => `<div class="listItems">
+                <label for="task${info.index}">
+                    <input id="task${info.index}" type="checkbox"> ${info.description}
+                </label>
+               </div>`,
+    );
+
+    // Each item in the array created by task function are joined and put inside the tasksContainer
+    tasksContainer.innerHTML = task.join('');
+  }
+
+  render = () => {
+    addBtn.addEventListener('click', this.add);
+  }
+}
