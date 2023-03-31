64e8155b23e17c4b099fdc8ed4b442e74d0a2cff
anbehindY <yanminthwin@gmail.com>
2023-03-31 11:42:15 +0630

    Modify add and remove function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import TaskList from './mainFunction.js';
 
 const taskList = new TaskList();
 taskList.render();
+
+const EditFunction = () => {
+  const formsElements = document.querySelectorAll('.formel');
+
+  formsElements.forEach((formel, index) => {
+    formel.addEventListener('input', (e) => {
+      e.preventDefault();
+      editTask(todoArray, index, e.target.value);
+    });
+  });
+};
--- src/mainFunction.js
+++ src/mainFunction.js
@@ -4,54 +4,58 @@ const addText = document.querySelector('#starter');
 
 export default class TaskList {
   constructor() {
-    this.tasksInfo = [];
+    this.tasksInfo = [] || JSON.parse(localStorage.getItem(this.taskKey));
     this.taskKey = 'key';
   }
 
   add = () => {
     if (addText.value) {
-      const description = addText.value;
-      const completed = false;
-      const index = 0;
       const taskInfo = {
-        description,
-        completed,
-        index,
+        description: addText.value,
+        completed: false,
+        index: this.tasksInfo.length + 1,
       };
       this.tasksInfo.push(taskInfo);
       addText.value = '';
       this.display();
+      this.saveTasks();
     }
-    addBtn.addEventListener('click', this.add);
+    addBtn.addEventListener('click', (this.add));
   }
 
   remove = () => {
-    tasksContainer.addEventListener('click', (event) => {
-      if (event.target.classList.contains('fa-trash-can')) {
-        const index = event.target.classList[1].split('-')[1];
+    tasksContainer.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-trash-can')) {
+        const index = e.target.classList[1].split('-')[1];
         this.tasksInfo.splice(index, 1);
         this.display();
+        this.saveTasks();
       }
     });
   }
 
-  display = () => {
-    //  Each item in the tasksInfo array passed through this function and a new array is created
-    const task = this.tasksInfo.map(
-      (info) => `<div class="listItems">
-                <label for="task${info.index}">
-                <input id="task${info.index}" type="checkbox"> ${info.description}</label>
+   display = () => {
+     //  Each item in the tasksInfo array passed through this function and a new array is created
+     const task = this.tasksInfo.map(
+       (info) => `<div class="taskItems" id="${info.index}">
+                <i id="task${info.index}" class="fa-regular fa-square"></i>
+                <label id="label${info.index}" class="labels">${info.description}</label>
                 <i class="fa-solid fa-trash-can"></i>
                 <i class="fa-solid fa-ellipsis fa-rotate-90"></i>
                </div>`,
-    );
+     );
 
-    // Each item in the array created by task function are joined and put inside the tasksContainer
-    tasksContainer.innerHTML = task.join('');
+     // Each item in the array created by task function are joined and put inside the tasksContainer
+     tasksContainer.innerHTML = task.join('');
+   }
+
+  saveTasks = () => {
+    localStorage.setItem(this.taskKey, JSON.stringify(this.tasksInfo));
   }
 
   render = () => {
-    this.remove();
     this.add();
+    this.remove();
+    this.edit();
   }
 }
