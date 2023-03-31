52996b8e1f0f24f5977168b56bb069a8c405af10
anbehindY <yanminthwin@gmail.com>
2023-04-01 01:53:42 +0630

    Initiate render function from TaskList class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import './styles.css';
 import TaskList from './mainFunction.js';
 
-const taskList = new TaskList();
-taskList.render();
-
-const EditFunction = () => {
-  const formsElements = document.querySelectorAll('.formel');
-
-  formsElements.forEach((formel, index) => {
-    formel.addEventListener('input', (e) => {
-      e.preventDefault();
-      editTask(todoArray, index, e.target.value);
-    });
-  });
-};
+const init = new TaskList();
+init.render();
