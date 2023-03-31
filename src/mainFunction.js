e0c8a27413f9101faa20ee45bb61058b881e5e95
anbehindY <yanminthwin@gmail.com>
2023-04-01 01:54:33 +0630

    Add edit function and complete main functions

--- src/mainFunction.js
+++ src/mainFunction.js
@@ -1,61 +1,90 @@
-const tasksContainer = document.querySelector('.tasks');
-const addBtn = document.querySelector('.enter');
-const addText = document.querySelector('#starter');
-
-export default class TaskList {
+class TaskList {
   constructor() {
-    this.tasksInfo = [] || JSON.parse(localStorage.getItem(this.taskKey));
+    this.tasksContainer = document.querySelector('.tasks');
+    this.addBtn = document.querySelector('.enter');
+    this.addText = document.querySelector('#starter');
+    this.form = document.querySelector('form');
     this.taskKey = 'key';
+    this.tasksInfo = [] || JSON.parse(localStorage.getItem(this.taskKey));
+    this.render();
   }
 
-  add = () => {
-    if (addText.value) {
+  display() {
+    const task = this.tasksInfo.map(
+      (info) => `<div class="taskItems" id="div-${info.index}">
+                      <i id="task-${info.index}" class="fa-regular fa-square"></i>
+                      <input class='infoTask' type="text" id="infoTask" data-index="${info.index}" name="description" value="${info.description}" />
+                      <i class="fa-solid fa-trash-can" data-index="${info.index}"></i>
+                      <i class="fa-solid fa-ellipsis fa-rotate-90"></i>
+                    </div>`,
+    );
+    this.tasksContainer.innerHTML = task.join('');
+  }
+
+  saveTasks() {
+    localStorage.setItem(this.taskKey, JSON.stringify(this.tasksInfo));
+  }
+
+  add() {
+    if (this.addText.value) {
       const taskInfo = {
-        description: addText.value,
+        description: this.addText.value,
         completed: false,
-        index: this.tasksInfo.length + 1,
+        index: 0,
       };
       this.tasksInfo.push(taskInfo);
-      addText.value = '';
+      this.addText.value = '';
+      taskInfo.index += this.tasksInfo.length;
       this.display();
       this.saveTasks();
     }
-    addBtn.addEventListener('click', (this.add));
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.add();
+    });
+    this.addBtn.addEventListener('click', this.add.bind(this));
   }
 
-  remove = () => {
-    tasksContainer.addEventListener('click', (e) => {
+  remove() {
+    this.tasksContainer.addEventListener('click', (e) => {
       if (e.target.classList.contains('fa-trash-can')) {
-        const index = e.target.classList[1].split('-')[1];
-        this.tasksInfo.splice(index, 1);
-        this.display();
-        this.saveTasks();
+        const id = parseInt(e.target.getAttribute('data-index'), 10);
+        for (let i = 0; i < this.tasksInfo.length; i += 1) {
+          if (this.tasksInfo[i].index === id) {
+            this.tasksInfo.splice(i, 1);
+            this.display();
+            this.saveTasks();
+          }
+        }
       }
     });
   }
 
-   display = () => {
-     //  Each item in the tasksInfo array passed through this function and a new array is created
-     const task = this.tasksInfo.map(
-       (info) => `<div class="taskItems" id="${info.index}">
-                <i id="task${info.index}" class="fa-regular fa-square"></i>
-                <label id="label${info.index}" class="labels">${info.description}</label>
-                <i class="fa-solid fa-trash-can"></i>
-                <i class="fa-solid fa-ellipsis fa-rotate-90"></i>
-               </div>`,
-     );
-
-     // Each item in the array created by task function are joined and put inside the tasksContainer
-     tasksContainer.innerHTML = task.join('');
-   }
-
-  saveTasks = () => {
-    localStorage.setItem(this.taskKey, JSON.stringify(this.tasksInfo));
+  edit() {
+    this.tasksContainer.addEventListener('click', (e) => {
+      const taskItem = e.target.closest('.taskItems');
+      if (taskItem) {
+        const infoTask = taskItem.querySelector('.infoTask');
+        infoTask.addEventListener('blur', () => {
+          const newDescription = infoTask.value.trim();
+          if (newDescription) {
+            const id = parseInt(infoTask.dataset.index, 10);
+            const taskIndex = this.tasksInfo.findIndex((task) => task.index === id);
+            this.tasksInfo[taskIndex].description = newDescription;
+          }
+          this.display();
+          this.saveTasks();
+        });
+      }
+    });
   }
 
-  render = () => {
+  render() {
     this.add();
     this.remove();
+    this.display();
     this.edit();
   }
 }
+
+export default TaskList;
